a7fb98271dc8f9aad1f5e7176da35a032613c225
NodirFatxullayev <nfatxullayev@internet.ru>
2022-05-19 11:54:15 +0500

    secundi korsatadigan palkani stil va js code lari tayyor

---
+++ js/java.js
@@ -0,0 +1,12 @@
+// let secund = document.querySelector(".secund");
+
+
+
+// setInterval(s, (1/6)*1000);
+
+// function s() {
+//     console.log(new Date().getSeconds());
+    
+//     secund.style.transform = `rotate(${new Date().getSeconds()*6}deg)`;
+    
+// }
