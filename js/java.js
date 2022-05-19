3b14890c06cec34a90ae854eca3118793076779c
NodirFatxullayev <nfatxullayev@internet.ru>
2022-05-19 12:42:04 +0500

    minut palkaniyan style va script lari tayor

--- js/java.js
+++ js/java.js
@@ -1,12 +1,21 @@
-// let secund = document.querySelector(".secund");
+let secund = document.querySelector(".secund");
+let minut = document.querySelector(".minut");
 
 
 
-// setInterval(s, (1/6)*1000);
+setInterval(m,(1/6)*1000);
+setInterval(s, (1/6)*1000);
 
-// function s() {
-//     console.log(new Date().getSeconds());
+
+function s() {
+    // console.log(new Date().getSeconds());
     
-//     secund.style.transform = `rotate(${new Date().getSeconds()*6}deg)`;
+    secund.style.transform = `rotate(${new Date().getSeconds()*6}deg)`;
     
-// }
+}
+function m() {
+    // console.log(new Date().getMinutes());
+    minut.style.transform = `rotate(${new Date().getMinutes() * 6}deg)`;
+}
+
+
